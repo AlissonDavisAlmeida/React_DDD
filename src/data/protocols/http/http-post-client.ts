867a7e4565539a888280bb0bfe6857cd8c0d8f40
alisson <alissoneatena1@gmail.com>
2023-08-15 15:48:26 -0300

    feat: ensure RemoteAuthentication throws InvalidCredentialsError when statusCode is 401

--- src/data/protocols/http/http-post-client.ts
+++ src/data/protocols/http/http-post-client.ts
@@ -1,8 +1,10 @@
+import { type HttpResponse } from './http-response';
+
 export interface HttpPostParams {
   url: string
   body?: object
 }
 
 export interface HttpPostClient {
-  post: (params: HttpPostParams) => Promise<void>
+  post: (params: HttpPostParams) => Promise<HttpResponse>
 }
