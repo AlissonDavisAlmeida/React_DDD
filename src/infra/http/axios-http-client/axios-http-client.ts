fb75dd6548de25392c3118e0fd31a0aed9378e14
alisson <alissoneatena1@gmail.com>
2023-09-22 15:53:50 -0300

    feat: ensure AxiosHttpClient returns correct body and statusCode

--- src/infra/http/axios-http-client/axios-http-client.ts
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -1,8 +1,13 @@
-import { type HttpPostParams } from '@/data/protocols/http';
+import { type HttpResponse, type HttpPostParams } from '@/data/protocols/http';
 import axios from 'axios';
 
 export class AxiosHttpClient {
-  async post (params: HttpPostParams<any>) {
-    await axios.post(params.url, params?.body);
+  async post (params: HttpPostParams<any>): Promise<HttpResponse<any>> {
+    const result = await axios.post(params.url, params?.body);
+
+    return {
+      statusCode: result.status,
+      body: result.data
+    };
   }
 }
