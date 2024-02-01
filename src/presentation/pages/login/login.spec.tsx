7c749e6fc7a36deb707018f64f0d1f1bd711f233
alisson <alissoneatena1@gmail.com>
2024-02-01 11:40:58 -0300

    feat: ensure Login present valid state if password is valid

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -84,4 +84,14 @@ describe('Login Component', () => {
     expect(emailStatus.title).toBe('Tudo certo!');
     expect(emailStatus.textContent).toBe('🟢');
   });
+  test('should show valid password state if validation succeeds', () => {
+    const { sut, validationSpy } = makeSut();
+    validationSpy.errorMessage = '';
+    const passwordInput = sut.getByTestId('password');
+    fireEvent.input(passwordInput, { target: { value: faker.internet.password() } });
+
+    const passwordStatus = sut.getByTestId('password-status');
+    expect(passwordStatus.title).toBe('Tudo certo!');
+    expect(passwordStatus.textContent).toBe('🟢');
+  });
 });
