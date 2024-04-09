b985c3aa0b8f1617f0cd9e661ce8f0d70dd81b6a
alisson <alissoneatena1@gmail.com>
2024-04-09 13:46:45 -0300

    feat: ensure ValidationComposite returns the first error found

--- src/validation/composite/validation-composite.spec.ts
+++ src/validation/composite/validation-composite.spec.ts
@@ -2,6 +2,10 @@ import { faker } from "@faker-js/faker/locale/pt_BR";
 import { type FieldValidation } from "../protocols/field-validation";
 import { ValidationComposite } from "./validation-composite";
 
+const CONSTANTS = {
+  error_message: "any_error"
+};
+
 interface SutTypes {
   sut: ValidationComposite
   fieldName: string
@@ -24,7 +28,6 @@ function makeSut (): SutTypes {
   const fieldValidationSpy = makeFieldValidation(fieldName);
   const fieldValidationSpy2 = makeFieldValidation(fieldName);
   const fieldsValidationsSpy = [fieldValidationSpy, fieldValidationSpy2];
-  fieldValidationSpy2.error = new Error("any_error");
   const sut = new ValidationComposite(fieldsValidationsSpy);
 
   return {
@@ -36,10 +39,23 @@ function makeSut (): SutTypes {
 
 describe("ValidationComposite", () => {
   test("should return error if any validation fails", () => {
-    const { sut, fieldName } = makeSut();
+    const { sut, fieldName, fieldsValidationsSpy } = makeSut();
+
+    fieldsValidationsSpy[1].error = new Error(CONSTANTS.error_message);
 
     const error = sut.validate(fieldName, faker.lorem.word());
 
-    expect(error).toBe("any_error");
+    expect(error).toBe(CONSTANTS.error_message);
+  });
+
+  test("should return the first error if more than one validation fails", () => {
+    const { sut, fieldName, fieldsValidationsSpy } = makeSut();
+
+    fieldsValidationsSpy[0].error = new Error(CONSTANTS.error_message);
+    fieldsValidationsSpy[1].error = new Error(faker.lorem.word());
+
+    const error = sut.validate(fieldName, faker.lorem.word());
+
+    expect(error).toBe(CONSTANTS.error_message);
   });
 });
