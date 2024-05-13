b9cb6ad9383f3cedf470dd12b33811cd8d56a253
alisson <alissoneatena1@gmail.com>
2024-05-13 11:31:52 -0300

    test: ensure AxiosHttpClient return the correct status and body on failure

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -1,8 +1,8 @@
-import { AxiosHttpClient } from './axios-http-client';
-import { type MockAxios, mockAxios } from '@/infra/test';
-import { mockPostRequest } from '@/data/test';
+import { AxiosHttpClient } from "./axios-http-client";
+import { type MockAxios, mockAxios } from "@/infra/test";
+import { mockPostRequest } from "@/data/test";
 
-jest.mock('axios');
+jest.mock("axios");
 
 interface MakeSut {
   sut: AxiosHttpClient
@@ -18,17 +18,35 @@ const makeSut = (): MakeSut => {
   };
 };
 
-describe('Axios Http Client', () => {
-  test('should call axios with correct values', async () => {
+describe("Axios Http Client", () => {
+  test("should call axios with correct values", async () => {
     const { url, body } = mockPostRequest();
     const { sut, mockedAxios: { mockedAxios } } = makeSut();
     await sut.post({ url, body });
 
     expect(mockedAxios.post).toHaveBeenCalledWith(url, body);
   });
-  test('should return the correct statusCode and body', async () => {
+  test("should return the correct statusCode and body", async () => {
     const { url, body } = mockPostRequest();
     const { sut, mockedAxios: { mockResolvedValues } } = makeSut();
+    const result = await sut.post({ url, body });
+
+    expect(result).toEqual({
+      statusCode: mockResolvedValues.status,
+      body: mockResolvedValues.data
+    });
+  });
+  test("should return the correct statusCode and body on failure", async () => {
+    const { url, body } = mockPostRequest();
+    const { sut, mockedAxios: { mockResolvedValues, mockedAxios } } = makeSut();
+
+    mockedAxios.post.mockRejectedValueOnce({
+      response: {
+        status: mockResolvedValues.status,
+        data: mockResolvedValues.data
+      }
+    });
+
     const result = await sut.post({ url, body });
 
     expect(result).toEqual({
