725f51f2da6f2fa86cbac5b7f1fc4551ed0897c2
alisson <alissoneatena1@gmail.com>
2024-05-15 13:54:24 -0300

    feat: ensure LocalSaveAccessToken calls SetStorage with correct value

---
+++ src/data/useCases/save-access-token/local-save-access-token.spec.ts
@@ -0,0 +1,29 @@
+import { faker } from "@faker-js/faker/locale/pt_BR";
+import { LocalSaveAccessToken } from "./local-save-access-token";
+import { SetStorageSpy } from "@/data/test/mock-storage";
+
+interface SutTypes {
+  sut: LocalSaveAccessToken
+  setStorageSpy: SetStorageSpy
+}
+
+const makeSut = (): SutTypes => {
+  const setStorageSpy = new SetStorageSpy();
+  const sut = new LocalSaveAccessToken(setStorageSpy);
+
+  return {
+    sut,
+    setStorageSpy
+  };
+};
+
+describe("LocalSaveAccessToken", () => {
+  test("should call SetStorage with correct value", async () => {
+    const { sut, setStorageSpy } = makeSut();
+    const uuid = faker.string.uuid();
+    await sut.save(uuid);
+
+    expect(setStorageSpy.key).toBe("accessToken");
+    expect(setStorageSpy.value).toBe(uuid);
+  });
+});
