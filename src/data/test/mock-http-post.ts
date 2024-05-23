19324e6653468f8dd92872ce8fc2fde3d7920f8d
alisson <alissoneatena1@gmail.com>
2024-05-23 09:38:50 -0300

    refactor: move all mock-http to a single file

--- src/data/test/mock-http-post.ts
+++
@@ -1,9 +0,0 @@
-import { type HttpPostParams } from "../protocols/http";
-import { faker } from "@faker-js/faker/locale/pt_BR";
-
-export const mockPostRequest = (): HttpPostParams<any> => {
-  return {
-    url: faker.internet.url(),
-    body: faker.person.bio
-  };
-};
--- src/data/test/mock-http.ts
+++ src/data/test/mock-http.ts
@@ -1,4 +1,5 @@
 import { HttpStatusCode, type HttpPostClient, type HttpPostParams, type HttpResponse } from "@/data/protocols/http/";
+import { faker } from "@faker-js/faker/locale/pt_BR";
 
 export class HttpPostClientSpy<BodyRequest, ResponseBody> implements HttpPostClient<BodyRequest, ResponseBody> {
   url?: string;
@@ -13,3 +14,10 @@ export class HttpPostClientSpy<BodyRequest, ResponseBody> implements HttpPostCli
     return await Promise.resolve(this.response);
   }
 }
+
+export const mockPostRequest = (): HttpPostParams<any> => {
+  return {
+    url: faker.internet.url(),
+    body: faker.person.bio
+  };
+};
