7b2eb6c0a391f1c1f34eef084898ce6d35af555e
alisson <alissoneatena1@gmail.com>
2024-08-16 16:44:44 -0300

    refactor: move setCurrentAccountAdapter to own file

--- src/main/router/protected-route.tsx
+++ src/main/router/protected-route.tsx
@@ -1,16 +1,6 @@
-import React from "react";
-import { type AccountModel } from "@/domain/models";
 import { ApiContext } from "@/presentation/context/api/api-context";
 import { Outlet } from "react-router-dom";
-import { UnexpectedError } from "@/domain/errors";
-import { makeLocalStorageAdapter } from "../factories/cache/local-storage-adapter-factory";
-
-const setCurrentAccountAdapter = (account: AccountModel) => {
-  if (!account.token) {
-    throw new UnexpectedError();
-  }
-  makeLocalStorageAdapter().set("account", account);
-};
+import { setCurrentAccountAdapter } from "../adapter/current-account-adapter";
 
 export const ProtectedRoute = () => {
   return (
