c32a4de9f898a5a56300a7ed474be6683b10c285
alisson <alissoneatena1@gmail.com>
2024-08-19 11:45:34 -0300

    feat: ensure LocalStorageAdapter class localStorage.getItem with correct value

--- src/infra/cache/local-storage-adapter.spec.ts
+++ src/infra/cache/local-storage-adapter.spec.ts
@@ -10,11 +10,17 @@ describe("LocalStorageAdapter", () => {
     localStorage.clear();
   });
 
-  test("should call localStorage with correct values", async () => {
+  test("should call localStorage.setItem with correct values", async () => {
     const sut = makeSut();
     const value = "{\"value\":\"any_value\"}";
     const valueParsed = JSON.parse(value);
     sut.set("any_key", valueParsed);
     expect(localStorage.setItem).toHaveBeenCalledWith("any_key", value);
+  });
+  test("should call localStorage.getItem with correct value", async () => {
+    const sut = makeSut();
+    const value = "account";
+    sut.get(value);
+    expect(localStorage.getItem).toHaveBeenCalledWith(value);
   });
 });
--- src/infra/cache/local-storage-adapter.ts
+++ src/infra/cache/local-storage-adapter.ts
@@ -1,8 +1,14 @@
+import { type GetStorage } from "@/data/protocols/cache/get-storage";
 import { type SetStorage } from "@/data/protocols/cache/set-storage";
 
-export class LocalStorageAdapter implements SetStorage {
+export class LocalStorageAdapter implements SetStorage, GetStorage {
   set (key: string, value: object): void {
     const valueInString = JSON.stringify(value);
     localStorage.setItem(key, valueInString);
+  }
+
+  get (key: string): any {
+    const value = localStorage.getItem(key);
+    return value ? JSON.parse(value) : null;
   }
 }
