e23d6c5baffca0a1e469fbf934d3fb2d339b9ada
alisson <alissoneatena1@gmail.com>
2024-08-19 10:44:18 -0300

    test: ensure CurrentAccountAdapter calls LocalStorageAdapter with correct values

--- src/domain/usecases/index.ts
+++ src/domain/usecases/index.ts
@@ -1,3 +1,2 @@
 export * from "./account/authentication";
 export * from "./account/add-account";
-export * from "./account/save-current-account";
---
+++ src/main/adapter/current-account-adapter.spec.ts
@@ -0,0 +1,20 @@
+import { mockAccountModel } from "@/domain/test";
+import { setCurrentAccountAdapter } from "./current-account-adapter";
+import { LocalStorageAdapter } from "@/infra/cache/local-storage-adapter";
+
+// jest.mock("@/infra/cache/local-storage-adapter");
+describe("CurrentAccountAdapter", () => {
+  test("should call LocalStorageAdapter with correct values", () => {
+    const localStorageSpy = jest.spyOn(LocalStorageAdapter.prototype, "set");
+    const account = mockAccountModel();
+    setCurrentAccountAdapter(account);
+
+    expect(localStorageSpy).toHaveBeenCalledWith("account", account);
+  });
+
+  test("should throw if an invalid account is provided", () => {
+    const account = mockAccountModel();
+    account.token = "";
+    expect(() => { setCurrentAccountAdapter(account); }).toThrow(new Error("Invalid token"));
+  });
+});
